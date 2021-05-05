d4883a375e4365f2b6cfc2cf75455dc2c34406e2
Victor Cruz <vicdan215@gmail.com>
2021-05-04 19:14:33 -0500

    Enhance model construction & image methods

--- cnn.py
+++ cnn.py
@@ -7,21 +7,134 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from image import Image
-from constants import classnames
+from constants import classnames, folder
 
-image = Image(gray=True)
 
-# Basic tensorflow layers for grayscale images
-model = keras.Sequential()
-model.add(keras.layers.Flatten(input_shape=image.getSizeImage()))
-model.add(keras.layers.Dense(128, activation='relu'))
-model.add(keras.layers.Dense(len(classnames), activation='softmax'))
+class Covnet():
+    """
+    Class for a CNN implementation with CIFAR10
+    """
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    def __init__(self):
+        self.image = Image()
+        self.buildModel()
 
-images, labels = image.getTrainSet()
-model.fit(images, labels, epochs=10)
+    def buildModel(self):
+        """
+        Generate a model with different layers
+        """
+        # Basic tensorflow layers for grayscale images
+        self.model = keras.Sequential()
+        # CNN Layers
+        self.model.add(keras.layers.Conv2D(32, (3, 3), activation='relu',
+                                           input_shape=self.image.getSizeImage()))
+        self.model.add(keras.layers.Conv2D(32, (3, 3), activation='relu'))
+        self.model.add(keras.layers.MaxPool2D((2, 2)))
+        self.model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(keras.layers.MaxPool2D((2, 2)))
+        # Dense Layers
+        self.model.add(keras.layers.Flatten())
+        self.model.add(keras.layers.Dense(128, activation='relu'))
+        self.model.add(keras.layers.Dense(
+            len(classnames), activation='softmax'))
 
-images, labels = image.getTestSet()
-testLoss, testAcc = model.evaluate(images, labels, verbose=2)
-print('\nTest accuracy:', testAcc)
+        self.model.compile(optimizer='adam',
+                           loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+        self.model.summary()
+
+    def trainModel(self):
+        """
+        Fit & evaluate the model
+        """
+        trainImages, trainLabels = self.image.getTrainSet()
+        testImages, testLabels = self.image.getTestSet()
+        history = self.model.fit(
+            trainImages, trainLabels, epochs=10, validation_data=(testImages, testLabels))
+        self.summarizeDiagnostic(history)
+
+    def summarizeDiagnostic(self, history):
+        """
+        Plot loss & accuracy of the model
+        """
+        # Loss
+        plt.subplot(2, 1, 1)
+        plt.title('Cross Entropy Loss')
+        plt.plot(history.history['loss'], color='blue', label='train')
+        plt.plot(history.history['val_loss'], color='orange', label='test')
+        # Accuracy
+        plt.subplot(2, 1, 2)
+        plt.title('Classification Accuracy')
+        plt.plot(history.history['accuracy'], color='blue', label='train')
+        plt.plot(history.history['val_accuracy'], color='orange', label='test')
+        # plt.show()
+        plt.savefig(folder + 'diagnostic.png')
+
+    def predictTest(self):
+        """
+        Make predictions with a test set
+        """
+        images, labels = self.image.getTestSet()
+        _, testAcc = self.model.evaluate(images, labels, verbose=2)
+        print('\nTest accuracy:', testAcc)
+
+        self.predictions = self.model.predict(images)
+        self.plotResults(images, labels)
+
+    def plotImage(self, i, predictions, trueLabel, img):
+        """
+        docstring
+        """
+        trueLabel, img = trueLabel[i], img[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(img, cmap=plt.cm.binary)
+
+        predictedLabel = np.argmax(predictions)
+        if predictedLabel == trueLabel:
+            color = 'blue'
+        else:
+            color = 'red'
+
+        plt.xlabel('{} {:2.0f}% ({})'.format(
+            classnames[predictedLabel], 100 * np.max(predictions), classnames[trueLabel], color=color))
+
+    def plotValueArray(self, i, predictions, trueLabel):
+        """
+        docstring
+        """
+        trueLabel = trueLabel[i]
+        plt.grid(False)
+        plt.xticks(range(10))
+        plt.yticks([])
+        thisplot = plt.bar(range(10), predictions, color='#777777')
+        plt.ylim([0, 1])
+        predictedLabel = np.argmax(predictions)
+
+        thisplot[predictedLabel].set_color('red')
+        thisplot[trueLabel].set_color('blue')
+
+    def plotResults(self, images, labels):
+        """
+        docstring
+        """
+        num_rows = 5
+        num_cols = 3
+        num_images = num_rows * num_cols
+        plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+        for i in range(num_images):
+            plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+            self.plotImage(i, self.predictions[i], labels, images)
+            plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+            self.plotValueArray(i, self.predictions[i], labels)
+        plt.tight_layout()
+        # plt.show()
+        plt.savefig(folder + 'results.png')
+
+
+if __name__ == "__main__":
+    cnn = Covnet()
+    cnn.buildModel()
+    cnn.trainModel()
+    cnn.predictTest()
--- image.py
+++ image.py
@@ -3,14 +3,15 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-from constants import classnames
+from constants import classnames, folder
+
 
 class Image():
     """
     Class for managing a image library from tensorflow.
     Specifically, we'll use CIFAR10
     """
-    
+
     def __init__(self, gray=False, showSamples=False):
         """
         Start getting the image by instantiate an Image object
@@ -19,7 +20,7 @@ class Image():
         if (gray):
             self.rgb2grayscale()
         if (showSamples):
-            self.showSamples()
+            self.showSamples(gray)
 
     def plotSample(self, index, gray=True):
         """
@@ -27,9 +28,9 @@ class Image():
         """
         if (gray):
             plt.imshow(self.trainImages[index], cmap=plt.cm.binary)
+            plt.colorbar()
         else:
             plt.imshow(self.trainImages[index])
-        plt.colorbar()
         plt.grid(False)
         plt.title(classnames[self.trainLabels[index]])
 
@@ -39,17 +40,22 @@ class Image():
         """
         print('Get images...')
         cifar = keras.datasets.cifar10
-        (self.trainImages, trainLabels), (self.testImages, testLabels) = cifar.load_data()
+        (self.trainImages, trainLabels), (self.testImages,
+                                          testLabels) = cifar.load_data()
+        self.trainImages = self.trainImages.astype('float32') / 255
+        self.testImages = self.testImages.astype('float32') / 255
         # Labels form: [[1], [2], [3], ...]
-        self.trainLabels = trainLabels.reshape(-1, ) # From 2D to 1D array
-        self.testLabels = testLabels.reshape(-1, ) # From 2D to 1D array
+        self.trainLabels = trainLabels.reshape(-1, )  # From 2D to 1D array
+        self.testLabels = testLabels.reshape(-1, )  # From 2D to 1D array
         # Labels form: [1, 2, 3, ...]
 
         self.sizeImage = self.trainImages[0].shape
 
         print('=== Summary ===')
-        print('Dimensions of train image set: {0}'.format(self.trainImages.shape))
-        print('Dimensions of test image set: {0}'.format(self.testImages.shape))
+        print('Dimensions of train image set: {0}'.format(
+            self.trainImages.shape))
+        print('Dimensions of test image set: {0}'.format(
+            self.testImages.shape))
         print('Total train labels: {0}'.format(len(self.trainLabels)))
         print('Total test labels: {0}'.format(len(self.testLabels)))
         print('Image size: {0}'.format(self.sizeImage))
@@ -66,22 +72,25 @@ class Image():
         plt.subplot(1, 2, 1)
         self.plotSample(example, gray=False)
 
-        self.trainImages = np.mean(self.trainImages, axis=3) / 255
-        self.testImages = np.mean(self.testImages, axis=3) / 255
+        self.trainImages = np.mean(self.trainImages, axis=3)
+        self.testImages = np.mean(self.testImages, axis=3)
 
         self.sizeImage = self.trainImages[0].shape
 
-        print('Dimensions of train image set: {0}'.format(self.trainImages.shape))
-        print('Dimensions of test image set: {0}'.format(self.testImages.shape))
+        print('Dimensions of train image set: {0}'.format(
+            self.trainImages.shape))
+        print('Dimensions of test image set: {0}'.format(
+            self.testImages.shape))
 
         print('Image size: {0}'.format(self.sizeImage))
 
         print('=== Transform to grayscale===')
         plt.subplot(1, 2, 2)
         self.plotSample(example)
-        plt.show()
+        # plt.show()
+        plt.savefig(folder + 'gray.png')
 
-    def showSamples(self):
+    def showSamples(self, gray):
         """
         Display 25 subplots of the train image set
         """
@@ -91,8 +100,9 @@ class Image():
             plt.subplot(5, 5, i + 1)
             plt.xticks([])
             plt.yticks([])
-            self.plotSample(i)
-        plt.show()
+            self.plotSample(i, gray)
+        # plt.show()
+        plt.savefig(folder + 'samples.png')
 
     def getTrainSet(self):
         """
@@ -115,6 +125,7 @@ class Image():
 # model = keras.Sequential()
 # model.add(keras.layers.Flatten(input_shape=size))
 # print(model.output_shape)
+
 
 if __name__ == "__main__":
     image = Image(showSamples=True)
